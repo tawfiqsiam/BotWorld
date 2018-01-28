553c8f07330a433fbcbdd787dbbdbf863f22b736
August (Chris) <27751995+ohlookitsAugust@users.noreply.github.com>
2018-01-27 17:30:45 -0700

    command

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,20 +1,24 @@
 const Discord = require('discord.js');
-const fs = require('fs');
-const config = require(`./../../config.json`);
 const yt = require('ytdl-core');
 
-module.exports.run = function(bot, command, args, message, updateJSON, addFooter){
-    if(bot.dispatchers.get(message.guild.members.get(message.author.id).voiceChannel.id)){
-        bot.dispatchers.set(message.guild.members.get(message.author.id).voiceChannel.id, function(){
-            const stream = yt(args[0], { filter : 'audioonly' });
-            let dispatcher = message.guild.members.get(message.author.id).voiceChannel.connection.playStream(stream, { seek: 0, volume: 1 });
-            return {dispatcher, stream};
-        });
-    }
-}
+module.exports.run = (bot, command, args, message, updateJSON, addFooter) => {
+    if (!args[0]) return message.channel.send('You will need to specify a link!');
+    const vc = message.member.voiceChannel;
+    
+    if (!vc) return message.channel.send('Please join a voice chat.');
+    
+    vc.join((connection) => {
+        const stream = yt(args[0], { filter: audioonly });
+        const dispatcher = connection.playStream(stream);
+        
+        yt.getInfo(args[0], (err, info) => message.channel.send(`Now playing **${info.title}**!`));
+        
+        dispatcher.on('end', async() => await vc.leave());
+    });
+};
 
 module.exports.help = {
     names: ['play'],
     usage: 'e-command <args>',
     description: 'what?'
-}
+};
