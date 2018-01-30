fd7f29ca2c2e0e19de4bb3a959995abe62a30725
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2018-01-29 20:00:08 -0500

    Update music.js

--- commands/music/music.js
+++ commands/music/music.js
@@ -6,17 +6,19 @@ const yt = require('ytdl-core');
 
 var opts = {
   maxResults: 1,
-  key: ''
+  key: 'youtube v3 API key'
 };
 let dispatcher;
 function play(message, song){
     if(song!=undefined){
         message.channel.send(`Playing song **${song[0].title}**`);
-        let stream = yt(song[0].link, { filter : 'audioonly', passes:1 });
+        let stream = yt(song[0].link, { filter : 'audioonly', passes:3 });
         dispatcher = message.guild.voiceConnection.playStream(stream, { seek: 0, volume: 1 });
-        dispatcher.on('end', function(){
+        dispatcher.once('end', function(){
+          setTimeout(function(){
             queue[message.guild.id].shift();
             play(message, queue[message.guild.id][0]);
+          }, 500);
         });
     }else{
         message.channel.send('No more songs, why not add more using e-queue <title>, and e-music play to start them :D.');
@@ -64,10 +66,12 @@ module.exports.run = function(bot, command, args, message, updateJSON, addFooter
   }
 
   if(args[0]=='list'){
+    console.log(queue[message.guild.id][0][0].title)
       let out = 'Queue: \n';
-      for(var i=0;i<queue[message.guild.id].size;i++){
+      for(var i=0;i<queue[message.guild.id].length;i++){
           out += queue[message.guild.id][i][0].title + '\n';
       }
+    message.channel.send(out);
   }
 }
 
